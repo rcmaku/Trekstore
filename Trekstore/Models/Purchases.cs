edb0670544dfd42f8bb95d4c3dc4136126ae4730
Jose Rodolfo Prado Ramirez <jose02_003@hotmail.com>
2024-06-24 05:58:46 -0600

    Add project files.

---
+++ Trekstore/Models/Purchases.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Trekstore.Models
+{
+    public class Purchases
+    {
+        [Key] public int PurchaseID { get; set; }
+
+        public int Amount { get; set; }
+    }
+}
---
+++ Trekstore/Models/Sales.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Trekstore.Models
+{
+    public class Sales
+    {
+        [Key] public int SalesID { get; set; }
+
+        public int Amount { get; set; }
+
+        [Required]
+        public int ClientID {  get; set; }
+        
+        public virtual Client? Clients { get; set; }
+
+        [Required]
+        public int ProductID {  get; set; }
+        public virtual Products? Product { get; set; }
+    }
+}
